1e8033c523847261cce91810a3ac162c988e119a
Kenyon Ralph <kenyon@kenyonralph.com>
2021-01-25 00:10:32 -0800

    spec/spec_helper_local.rb: beautify

--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
-CHECKCONF_CMD = '/usr/sbin/named-checkconf %'
-
 def checkzone_cmd(zone_name)
   "/usr/sbin/named-checkzone -k fail -m fail -M fail -n fail -r fail -S fail '#{zone_name}' %"
 end
 
+CHECKCONF_CMD = '/usr/sbin/named-checkconf %'
 CONFIG_DIR = File.join('/etc', 'bind')
 CONFIG_FILENAME = 'named.conf'
 CONFIG_FILE = File.join(CONFIG_DIR, CONFIG_FILENAME)
