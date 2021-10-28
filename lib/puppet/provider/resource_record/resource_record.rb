3c958b150d2c6e07c6d1b49638e34dcc05a87a29
griggi <griggi@clarkson.edu>
2021-10-28 14:24:05 -0400

    PTR generation debug prints

--- lib/puppet/provider/resource_record/resource_record.rb
+++ lib/puppet/provider/resource_record/resource_record.rb
@@ -85,6 +85,8 @@ class Puppet::Provider::ResourceRecord::ResourceRecord < Puppet::ResourceApi::Si
 
   def update(context, name, should)
     context.notice("Updating '#{name}' with #{should.inspect}")
+    context.debug("old: #{name.inspect}")
+    context.debug("new: #{should.inspect}")
     cmd = "echo 'zone #{should[:zone]}
     update delete #{name[:record]} #{name[:type]} #{name[:data]}
     update add #{should[:record]} #{should[:ttl]} #{should[:type]} #{should[:data]}
