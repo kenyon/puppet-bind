5a418fe20061292b032f8c404b21f5888980b3d6
griggi <griggi@clarkson.edu>
2021-11-15 09:28:15 -0500

    Define resource record title to always match that generated by provider get

--- manifests/zone.pp
+++ manifests/zone.pp
@@ -210,7 +210,7 @@ define bind::zone (
     }
 
     $resource_records.each |$rrname, $attribs| {
-      resource_record { "${attribs[record]} ${zone_name} ${attribs[type]} ${attribs[data]}":
+      resource_record { "${attribs['record']} ${zone_name} ${attribs['type']} ${attribs['data']}":
         zone => $zone_name,
         *    => $attribs,
       }
